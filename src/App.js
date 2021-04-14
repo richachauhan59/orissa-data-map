e827d08b7fb1deeb84bd8a842ddbc6dc3f876baa
richachauhan59 <richachauhan2019@gmail.com>
2021-04-14 13:21:53 +0530

    Minor Modifications done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
 // import Map from './Map';
 import mapData from "./data/map.json";
-import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
+import { MapContainer, GeoJSON, TileLayer, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./App.css";
 import * as d3 from "d3";
 import mainData from "./data/data_mgnregs_odisha_2019-20_pc_expenditure.csv";
+import { tooltip } from "leaflet";
+// import { Tooltip } from "leaflet";
+
+const mData = [];
+  d3.csv(mainData, function (mainData) {
+    mData.push(mainData);
+    console.log(mainData)
+
+  });
 
 function App() {
   const [data, setData] = useState([]);
   const [flag, setFlag] = useState(false);
 
-  const mData = [];
-  d3.csv(mainData, function (mainData) {
-    mData.push(mainData);
-  });
+  
 
   const geojsonStyle = {
     fillColor: "red",
@@ -23,7 +29,8 @@ function App() {
   const onEachRegion = (region, layer) => {
     const regionName = region.properties.pc_name;
     console.log();
-    layer.bindPopup(regionName);
+    // layer.bindTooltip(regionName, {permanent:true, sticky:true, className:"tooltip"});
+    layer.bindTooltip(regionName)
     layer.on("click", function (e) {
       mData.map(
         (item) =>
@@ -71,17 +78,15 @@ function App() {
             }}
             zoom={7}
             center={[20, 85]}
+            minZoom={7}
+            maxZoom={8}
           >
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <GeoJSON
-              eventHandlers={{
-                click: () => {
-                  <div>Richa</div>;
-                },
-              }}
+              // pane={mapData.features.properties.pc_name}
               style={geojsonStyle}
               data={mapData.features}
               onEachFeature={onEachRegion}
@@ -98,35 +103,41 @@ function App() {
                 </tr>
                 <tr>
                   <td>Opening Balance</td>
-                  <td>{data["Opening Balance"]}</td>
+                  <td>{data["Opening Balance"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Total Fund Available</td>
-                  <td>{data["Total Fund Available"]}</td>
+                  <td>{data["Total Fund Available"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Total Expenditure on Wages</td>
-                  <td>{data["Total Expenditure on Wages"]}</td>
+                  <td>{data["Total Expenditure on Wages"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Total Expenditure on Materials</td>
-                  <td>{data["Total Expenditure on Materials"]}</td>
+                  <td>{data["Total Expenditure on Materials"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Grand Total Expenditure</td>
-                  <td>{data["Grand Total Expenditure"]}</td>
+                  <td>{data["Grand Total Expenditure"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Total Unspent Balance</td>
-                  <td>{data["Total Unspent Balance"]}</td>
+                  <td>{data["Total Unspent Balance"].split("â¹")}</td>
                 </tr>
                 <tr>
                   <td>Total Payment Due</td>
-                  <td>{data["Total Payment Due"]}</td>
+                  <td>{data["Total Payment Due"].split("â¹")}</td>
                 </tr>
               </table>
             </div>
-          ) : null}
+          ) : 
+          <div style={{textAlign:"center"}}>
+            <h1>
+              Click on any District to check the  Expenditures for MGNREGS in parliamentary constituencies of Odisha
+            </h1>
+          </div>
+          }
         </div>
       </div>
     </div>
